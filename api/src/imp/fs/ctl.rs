9843a5feda697fe09982986622cc2ee8b6e77b58
vincent <songjh22@mails.tsinghua.edu.cn>
2025-04-05 01:51:38 +0800

    remove unimplemented sys_utimensat function from ctl.rs and its reference in syscall dispatcher

--- api/src/imp/fs/ctl.rs
+++ api/src/imp/fs/ctl.rs
@@ -303,12 +303,3 @@ pub fn sys_unlink(_path: UserConstPtr<c_char>) -> LinuxResult<isize> {
     Ok(0)
 }
 
-pub fn sys_utimensat(
-    dir_fd: i32,
-    path: UserConstPtr<c_char>,
-    times: usize,
-    _flags: i32,
-) -> LinuxResult<isize> {
-    warn!("[sys_unlink] not implemented yet");
-    Ok(0)
-}
