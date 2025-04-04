c8b5e1ed8a2eabfc7eb3dbd5ccac6e5c9b27083b
vincent <songjh22@mails.tsinghua.edu.cn>
2025-04-05 02:11:29 +0800

    remove unused imports and simplify sys_dup function in fd_ops.rs

--- api/src/imp/fs/fd_ops.rs
+++ api/src/imp/fs/fd_ops.rs
@@ -1,17 +1,9 @@
 use core::ffi::c_int;
 
 use arceos_posix_api as api;
-use arceos_posix_api::FD_TABLE;
-use axerrno::{LinuxError, LinuxResult};
-use axtask::{current, TaskExtRef};
+use axerrno::LinuxResult;
 
 pub fn sys_dup(old_fd: c_int) -> LinuxResult<isize> {
-    let curr = current();
-    let task = curr.task_ext();
-    if FD_TABLE.read().count()  >= task.get_rlimit_nofile().rlim_cur as usize{
-        return Err(LinuxError::EMFILE);
-     }
-
     Ok(api::sys_dup(old_fd) as _)
 }
 
